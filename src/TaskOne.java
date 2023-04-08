5d7eec50f16e0cb407c7ea3c77f2736648a85149
Bakhytgali <129316187+Bakhytgali@users.noreply.github.com>
2023-04-08 19:56:10 +0600

    Update TaskOne.java

--- src/TaskOne.java
+++ src/TaskOne.java
@@ -5,20 +5,24 @@ public class TaskOne {
         Scanner sc = new Scanner(System.in);
         System.out.println( "You are testing a task #1 \n" +
                             "Enter the length of an array: ");
-        int l = sc.nextInt(); /* Размер массива */
-        int[] array = new int[l];
+        int l = sc.nextInt(); /* l -  Length of the array */
+        int[] array = new int[l]; /* Creating array with length 'l' */
         System.out.println("Enter the elements of an array: ");
-        for(int i = 0; i < l; i++){
+        for(int i = 0; i < l; i++){ /* Entering the elements of the array using 'for loop' */
             array[i] = sc.nextInt();
         }
-        System.out.println("Minimum value is " + findMin(array, l) +".");
+        System.out.println("Minimum value is " + findMin(array, l) +"."); /* Sending to function
+                                                                            the array and it's length */
     }
-    public static int findMin(int[] array, int l){
+    public static int findMin(int[] array, int l){ /* Function that finds minimum element of the array
+                                                    @ int[] array - array form the main function
+                                                    @ int l - length of the array
+                                                    */
         if(l == 1){
             return array[0];
         } else {
-            int min = findMin(array, l-1);
-            return Math.min(min, array[l-1]);
+            int min = findMin(array, l-1); 
+            return Math.min(min, array[l-1]); /* Finding the minimum between 2 elements using Math.min() function */
         }
     }
 }
