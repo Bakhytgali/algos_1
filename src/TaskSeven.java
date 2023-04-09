5a3b0306c1f0f3b2e289a489c0fb401e9e4eae24
Bakhytgali <129316187+Bakhytgali@users.noreply.github.com>
2023-04-09 17:33:28 +0600

    Update TaskSeven.java

--- src/TaskSeven.java
+++ src/TaskSeven.java
@@ -4,15 +4,18 @@ public class TaskSeven {
         Scanner sc = new Scanner(System.in);
         System.out.println( "You are testing a task #7 \n" +
                             "Enter the length of an array: ");
-        int l = sc.nextInt();
-        int[] array = new int[l];
+        int l = sc.nextInt(); /* Entering the length of an array */
+        int[] array = new int[l]; /* Creating an array with the length l */
         System.out.println("Enter the elements of an array: ");
-        for(int i = 0; i < l; i++){
+        for(int i = 0; i < l; i++){ /* Entering the elements of an array using `for loop` */
             array[i] = sc.nextInt();
         }
         System.out.println("The reversed array: " + reverseArr(array, l));
     }
-    public static String reverseArr(int[] array, int l){
+    public static String reverseArr(int[] array, int l){ /*  reverseArr() function returns reversed array as a String
+                                                             int[] array - is an array from the main function
+                                                             int l - is length of an array
+                                                           */
         if(l == 1){
             return Integer.toString(array[0]);
         } else {
