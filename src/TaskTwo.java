8024df4af6efeed41158923a55866c9dd186c1ca
Bakhytgali <129316187+Bakhytgali@users.noreply.github.com>
2023-04-09 16:22:46 +0600

    Update TaskTwo.java

--- src/TaskTwo.java
+++ src/TaskTwo.java
@@ -4,22 +4,25 @@ public class TaskTwo {
         Scanner sc = new Scanner(System.in);
         System.out.println( "You are testing a task #2 \n" +
                             "Enter the length of an array: ");
-        int l = sc.nextInt();
-        int[] array = new int[l];
+        int l = sc.nextInt(); /* Entering the length of an array */
+        int[] array = new int[l]; /* Creating an array with the length l */
         System.out.println("Enter the elements of an array: ");
-        for(int i = 0; i < l; i++){
+        for(int i = 0; i < l; i++){ /* Entering the elements of an array using `for loop` */
             array[i] = sc.nextInt();
         }
-        float avg = findSum(array, l) / l ; /* Делю сумму на размер массива */
+        float avg = findSum(array, l) / l ; /* Dividing the sum of the elements by the length to get the average */
         System.out.println("The average of these elements is " + avg + ".");
     }
-    public static float findSum(int[] array, int l){ /* Функция для нахождения суммы элементов массива */
+    public static float findSum(int[] array, int l){ /*
+                                                       @ int[] array - array from the main function
+                                                       @ int l - array's length
+                                                     */
         if (l == 1){
             return array[0];
         } else {
-            findSum(array, l-1);
-            return array[0] += array[l-1]; /* Прибавляю к первому элементу последний
-                                            и присваиваю это значение обратно первому элементу */
+            findSum(array, l-1); 
+            return array[0] += array[l-1]; /* Summing the last element with the first 
+                                            and declaring the result as a new first element */
         }
     }
 }
