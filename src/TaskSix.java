72ad52ecb50226e9038db138a1161d69e523e82b
Bakhytgali <129316187+Bakhytgali@users.noreply.github.com>
2023-04-09 16:47:58 +0600

    Update TaskSix.java

--- src/TaskSix.java
+++ src/TaskSix.java
@@ -4,16 +4,16 @@ public class TaskSix {
         Scanner sc = new Scanner(System.in);
         System.out.println( "You are testing a task #6 \n" +
                             "Enter the value of a: ");
-        int a = sc.nextInt();
+        int a = sc.nextInt(); /*  int a - number */
         System.out.println("Enter the value of n: ");
-        int n = sc.nextInt();
-        System.out.println("The result (a in the power of n) is: " + power(a, n));
+        int n = sc.nextInt(); /* int n - power of a */
+        System.out.println("The result (a in the power of n) is: " + findPower(a, n));
     }
-    public static long power(int a, int n){
+    public static long findPower(int a, int n){ /* findPower() - function to find n-th power of number a */
         if(n == 0){
             return 1;
         } else {
-            return a * power(a, n-1);
+            return a * findPower(a, n-1); /* Multiplicating the number a for itself n times */
         }
     }
 }
