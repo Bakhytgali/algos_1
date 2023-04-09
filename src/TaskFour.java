03ac0d2cb5df9e24e9e2221a2fde1843e05fa0c5
Bakhytgali <129316187+Bakhytgali@users.noreply.github.com>
2023-04-09 16:34:12 +0600

    Update TaskFour.java

--- src/TaskFour.java
+++ src/TaskFour.java
@@ -4,14 +4,15 @@ public class TaskFour {
         Scanner sc = new Scanner(System.in);
         System.out.println( "You are testing a task #4 \n" +
                             "Enter the number: ");
-        int n = sc.nextInt();
+        int n = sc.nextInt(); /* int n - the number to define it's factorial */
         System.out.println("Factorial of this number is: " + factorial(n));
     }
-    public static int factorial(int n){
+    public static int factorial(int n){ /* factorial() - function to find factorial
+                                           @ int n - the number from the main function */
         if(n == 0){
             return 1;
         } else {
-            return n * factorial(n-1);
+            return n * factorial(n-1); /* multiplicating the number n with the all previous numbers  */
         }
     }
 }
