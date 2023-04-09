3a7144f60209b1f7a5e7815941b0cb1a9b37f7b7
Bakhytgali <129316187+Bakhytgali@users.noreply.github.com>
2023-04-09 16:28:21 +0600

    Update TaskThree.java

--- src/TaskThree.java
+++ src/TaskThree.java
@@ -4,19 +4,19 @@ public class TaskThree {
         Scanner sc = new Scanner(System.in);
         System.out.println( "You are testing a task #3 \n" +
                             "Enter the number: ");
-        int n = sc.nextInt();
-        System.out.println(isPrime(n) ? "Yes":"No");
+        int n = sc.nextInt(); /* int n - the number to check for prime */
+        System.out.println(isPrime(n) ? "Yes":"No"); /* Outputing result using ternary operator */
     }
-    public static boolean isPrime(int n){
-        boolean prime = false;
-        for(int i = 2; i < n; i++){
-            if(n%i == 0){
+    public static boolean isPrime(int n){ /* int n - the number from the main function */
+        boolean prime = false; /* boolean prime - variable that defines prime */
+        for(int i = 2; i < n; i++){ /* checking if the number divides by any number in range [2; n) */
+            if(n%i == 0){  
                 prime = false;
                 break;
             } else {
                 prime = true;
             }
         }
-        return(prime ? true:false);
+        return(prime ? true:false); /* Returning the answer using ternary operator */
     }
 }
